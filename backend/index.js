98e34acb98b4732c1d7901ff105b1de5252be3a8
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-01-11 00:01:59 +0530

    add the origin

--- backend/index.js
+++ backend/index.js
@@ -8,39 +8,33 @@ import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
 
-dotenv.config();
+dotenv.config({});
 
 const app = express();
 
 // middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
-
-// CORS options
 const corsOptions = {
-  origin: process.env.NODE_ENV === 'production'
-    ? 'https://job-portal-nine-theta.vercel.app'
-    : 'http://localhost:3000',  // Local development URL
-  credentials: true,
-};
+    origin:'https://job-portal-nine-theta.vercel.app/',
+    credentials:true
+}
 
 app.use(cors(corsOptions));
 
-const PORT = process.env.PORT || 8000; // Default port if not set
+const PORT = process.env.PORT  ;
 
-// API routes
+
+// api's
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/company", companyRoute);
 app.use("/api/v1/job", jobRoute);
 app.use("/api/v1/application", applicationRoute);
 
-// Start the server
-app.listen(PORT, () => {
-  connectDB();
-  console.log(`Server running at port ${PORT}`);
-});
-app.get('/', (req, res) => {
-    res.send('Welcome to the API!');
-  });
-  
+
+
+app.listen(PORT,()=>{
+    connectDB();
+    console.log(`Server running at port ${PORT}`);
+})
