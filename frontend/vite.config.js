049741e1f71ca9ebfc407483835413f793f68d74
Shubham53Mishra <21cse53@vmkvec.edu.in>
2025-01-10 23:33:44 +0530

    look

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,12 @@
-import { fileURLToPath } from "url";
-import { dirname, resolve } from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
-// Get the current directory path for ES module
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
