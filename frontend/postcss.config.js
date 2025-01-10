cd0639d2d6b3ee097055ff02d44cc0e3fe752104
Shubham tripathi <142476256+Shubham-tripathi031@users.noreply.github.com>
2025-01-10 22:32:01 +0530

    Update postcss.config.js

--- frontend/postcss.config.js
+++ frontend/postcss.config.js
@@ -1,6 +1,11 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+// postcss.config.js
+
+module.exports = {
+  // your postcss configuration goes here
+  plugins: [
+    // Example plugin setup
+    // eslint-disable-next-line no-undef
+    require('autoprefixer'),
+    // other plugins...
+  ]
 };
